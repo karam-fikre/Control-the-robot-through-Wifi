9933e84ea20cc2485f1c18cf6b0eb3eb372b6755
karamfikre <karam_love.aldolaimy@yahoo.com>
2017-11-15 10:44:00 +0100

    Add project files.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;  
+using System.Net;  
+using System.Net.Sockets;  
+using System.Text;  
+
+class Program   
+{  
+    static void Main(string[] args)   
+    {  
+        Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Dgram,  
+            ProtocolType.Udp);  
+
+        IPAddress broadcast = IPAddress.Parse("192.168.4.1");
+        while (true)
+        {
+            byte[] sendbuf = Encoding.ASCII.GetBytes(Console.ReadLine());
+            IPEndPoint ep = new IPEndPoint(broadcast, 1025);
+
+            s.SendTo(sendbuf, ep);
+        }
+        Console.WriteLine("Message sent to the broadcast address");  
+    }  
+} 
