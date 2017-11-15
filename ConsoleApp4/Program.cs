d8afd1caaa6b844f608a36330ced0963cf1d6e2d
karamfikre <karam_love.aldolaimy@yahoo.com>
2017-11-15 10:50:47 +0100

    Edit

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -11,8 +11,16 @@ class Program
             ProtocolType.Udp);  
 
         IPAddress broadcast = IPAddress.Parse("192.168.4.1");
+        Console.WriteLine("Type a number to move the robot:");
+        Console.WriteLine("1 : Forward");
+        Console.WriteLine("2 : Backward");
+        Console.WriteLine("3 : Left");
+        Console.WriteLine("4 : Right");
+        Console.WriteLine("5 : Stop");
+        Console.WriteLine("");
         while (true)
         {
+           
             byte[] sendbuf = Encoding.ASCII.GetBytes(Console.ReadLine());
             IPEndPoint ep = new IPEndPoint(broadcast, 1025);
 
